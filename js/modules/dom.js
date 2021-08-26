2e4cc397897681a928b5afd4ca12ba08569a57b7
Ilyas <ilyas.sultanov2020@list.ru>
2021-08-26 12:45:12 +0500

    dom changed

--- js/modules/dom.js
+++ js/modules/dom.js
@@ -15,14 +15,7 @@ let score = 0;
 let game;
 let speed;
 let snake = [];
-snake[0] = {
-    x: 9 * box,
-    y: 10 * box
-}
-let food = {
-    x: Math.floor(Math.random() * 17 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 3) * box
-}
+let food
 const ground = new Image();
 ground.src = "img/ground.png";
 

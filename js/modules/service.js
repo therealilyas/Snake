fa01ad2d0b4aeba33b6cb357b5ddd427f5c6f66a
Ilyas <ilyas.sultanov2020@list.ru>
2021-08-26 10:18:23 +0500

    startGame()

--- js/modules/service.js
+++ js/modules/service.js
@@ -71,6 +71,17 @@ function startGame() {
     document.body.style.background = 'black';
     DOM.startGameBox.style.display = 'flex';
 
+    DOM.dir = '';
+    DOM.snake = [];
+    DOM.snake[0] = {
+        x: 9 * DOM.box,
+        y: 10 * DOM.box
+    }
+    DOM.food = {
+        x: Math.floor(Math.random() * 17 + 1) * DOM.box,
+        y: Math.floor(Math.random() * 15 + 3) * DOM.box
+    }
+
 }
 
 
--- js/script.js
+++ js/script.js
@@ -8,16 +8,7 @@ init();
 function init() {
     startGame();
 
-    DOM.dir = '';
-    DOM.snake = [];
-    DOM.snake[0] = {
-        x: 9 * DOM.box,
-        y: 10 * DOM.box
-    }
-    DOM.food = {
-        x: Math.floor(Math.random() * 17 + 1) * DOM.box,
-        y: Math.floor(Math.random() * 15 + 3) * DOM.box
-    }
+
     EVENT.easy;
     EVENT.normal;
     EVENT.hard;

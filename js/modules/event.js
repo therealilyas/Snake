b80ceb654d0b2ddfb6f67444a7df481bb2c526b1
Ilyas <ilyas.sultanov2020@list.ru>
2021-08-26 09:46:53 +0500

    eventListener module bug fixed

---
+++ js/modules/event.js
@@ -0,0 +1,39 @@
+import DOM from '../modules/dom.js';
+import {
+    direction,
+    getLevelGame,
+    restartGame,
+
+} from '../modules/service.js'
+
+
+
+document.addEventListener('keydown', (e) => {
+    direction(e)
+});
+
+
+let easy = DOM.easyBtn.addEventListener('click', () => {
+    getLevelGame(200);
+
+});
+
+let normal = DOM.normalBtn.addEventListener('click', () => {
+    getLevelGame(100);
+
+});
+
+let hard = DOM.hardBtn.addEventListener('click', () => {
+    getLevelGame(80);
+});
+
+let restart = DOM.restartBtn.addEventListener('click', () => {
+    console.log("Restart")
+    restartGame()
+});
+export default {
+    easy,
+    normal,
+    hard,
+    restart
+}
--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-import { startGame, restartGame } from '../js/modules/service.js'
+import { startGame } from '../js/modules/service.js'
 import DOM from '../js/modules/dom.js';
 
-import {
-    direction,
-    getLevelGame,
-
-} from '../js/modules/service.js'
 
 init();
-startGame();
 
 function init() {
+    startGame();
+
     DOM.dir = '';
     DOM.snake = [];
     DOM.snake[0] = {
@@ -22,26 +18,3 @@ function init() {
         y: Math.floor(Math.random() * 15 + 3) * DOM.box
     }
 }
-
-
-document.addEventListener('keydown', (e) => { direction(e) });
-
-
-DOM.easyBtn.addEventListener('click', () => {
-    console.log("Easy")
-    getLevelGame(200);
-
-});
-
-DOM.normalBtn.addEventListener('click', () => {
-    getLevelGame(100);
-});
-
-DOM.hardBtn.addEventListener('click', () => {
-    getLevelGame(80);
-});
-
-DOM.restartBtn.addEventListener('click', () => {
-    console.log("Restart")
-    restartGame()
-});
